0d94181df25fdb18d76a6152d3964f93a99bc6fe
molghi <54518k@gmail.com>
2025-08-13 04:00:57 +0400

    initial commit, added Header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    // data:
+
+    logo: { [key: string]: string } = {
+        image: 'assets/images/icons/logo.svg',
+        link: '#',
+    };
+
+    navMenu: Array<any> = [
+        { title: 'Discover initiatives', link: '#' },
+        { title: 'Funded grants', link: '#' },
+        { title: 'Program expenses', link: '#' },
+        { title: 'Blog', link: '#' },
+        { title: 'FAQ', link: '#' },
+    ];
+
+    button: { [key: string]: string } = {
+        title: 'Apply <span>for grant</span>',
+        link: '#',
+    };
+
+    socials: Array<any> = [
+        { link: '#', image: 'assets/images/icons/twi.svg' },
+        { link: '#', image: 'assets/images/icons/diss.svg' },
+    ];
+}
